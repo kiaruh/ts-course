af5a895991f8017e4361c18d98cec7f146516e18
kiaruh <nikoz.li@gmail.com>
2023-01-12 19:29:16 -0300

    ts classes & more

--- basic2ts/src/index.ts
+++ basic2ts/src/index.ts
@@ -1,2 +1,28 @@
 const body = document.body
 body.innerHTML = '<style> body{background-color:#313131 } </style>'
+
+// class User {
+//     public email:string
+//     private name:string //and also you can use # -> #name
+//     readonly city:string = ''
+//     constructor(email:string, name:string){
+//         this.email = email,
+//         this.name = name
+//     }
+// }
+
+class User { //same code
+    
+    readonly city:string = ''
+    constructor(
+        public email:string,
+        public name:string
+        // private userID:string
+        ){
+    
+        this.email = email,
+        this.name = name
+    }
+}
+
+const nico = new User('n@i.co','nico')
