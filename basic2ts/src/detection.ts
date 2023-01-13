22d9080aa1f4c1a80dd3327f89747b50dcf5a4ee
kiaruh <nikoz.li@gmail.com>
2023-01-13 14:01:23 -0300

    more type narrowing + discrimination union

--- basic2ts/src/detection.ts
+++ basic2ts/src/detection.ts
@@ -1,3 +1,5 @@
+import { cA } from "chart.js/dist/chunks/helpers.core"
+
 function detectType(val:number|string){
     if(typeof val === 'string'){
         return val.toLowerCase()
@@ -31,5 +33,69 @@ function isAdminAccount(acc:User|Admin){
         return acc.isAdmin
     }
     return false
+}
 
+function logValue(x:Date|string){
+    if(x instanceof Date){
+        console.log(x.toUTCString())
+    }else{
+        console.log(x.toUpperCase())
+    }
+}
+
+type Fish = {swim:()=>void}
+type Bird = {fly:()=>void}
+
+function isFish(pet:Fish|Bird):pet is Fish{
+   return (pet as Fish).swim!==undefined
+}
+
+function feed(pet:Fish|Bird){
+    if(isFish(pet)){
+        return 'fish food'
+    }else{
+        return 'bird food'
+    }
+}
+
+interface Circle {
+    kind: 'circle'
+    radius: number
+}
+
+interface Square {
+    kind: 'square'
+    side:number
+}
+
+interface Rectangle {
+    kind: 'rectangle'
+    length:number
+    height:number
+}
+
+type Shape = Circle|Square|Rectangle
+
+function getTrueShape(shape:Shape){
+    if(shape.kind==='circle'){
+        return Math.PI * shape.radius **2
+    }
+    //return shape.side* shape.side
+}
+
+function getArea(shape:Shape){ //the never case
+    switch(shape.kind){
+        case 'circle':
+            return Math.PI * shape.radius **2
+
+        case 'square':
+            return shape.side* shape.side
+
+        case 'rectangle':
+            return shape.length*shape.height
+        
+        default: //this code helps me to check the types
+            const _defaultforshape:never=shape
+            return _defaultforshape
+    }
 }
