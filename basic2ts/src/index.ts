01e99de05e5b90bc0591255ce3b83548b01e8ac9
kiaruh <nikoz.li@gmail.com>
2023-01-12 22:25:57 -0300

    getter setters

--- basic2ts/src/index.ts
+++ basic2ts/src/index.ts
@@ -12,6 +12,8 @@ body.innerHTML = '<style> body{background-color:#313131 } </style>'
 // }
 
 class User { //same code
+
+    protected _courseCount = 1 //if its private, child extended classes cannot access to the property
     
     readonly city:string = ''
     constructor(
@@ -23,6 +25,33 @@ class User { //same code
         this.email = email,
         this.name = name
     }
+
+    private deleteToken(){
+        console.log('token deleted')
+    }
+
+    get getAppleEmail():string{
+        return `apple@s${this.email}`
+    }
+
+    get courseCount():number{
+        return this._courseCount
+    }
+    set courseCount(courseNum){
+        if(courseNum<=1){
+            throw new Error('Course count should be more than 1')
+        }
+        this._courseCount = courseNum
+    }
+
 }
 
 const nico = new User('n@i.co','nico')
+
+class SubUser extends User {
+    isFamily: boolean = true
+    changeCourseCountr(){
+        this._courseCount = 4
+    }
+
+}
